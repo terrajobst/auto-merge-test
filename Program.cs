9165d4efe546b179d4c52758c8271810695d591b
Immo Landwerth <immo@landwerth.net>
2021-03-23 12:50:32 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace auto_merge_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
